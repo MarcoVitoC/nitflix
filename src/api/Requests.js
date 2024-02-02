33993564e7ca4daee9e21237d8bf237eab60ba84
MarcoVitoC <marcovito.chr@gmail.com>
2024-02-02 23:35:34 +0800

    feat: add hero section and disable prop-types

--- src/api/Requests.js
+++ src/api/Requests.js
@@ -6,11 +6,14 @@ const requests = {
    nowPlayingMovies: `${baseUrl}/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`,
    trendingMovies: `${baseUrl}/trending/movie/day?api_key=${apiKey}&language=en-US&page=1`,
    upcomingMovies: `${baseUrl}/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`,
-   topRatedMovies: `${baseUrl}/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`
 };
 
 const getMovieImage = (path) => {
    return `${baseImgUrl}/${path}`;
 }
 
-export { requests, getMovieImage };
+const getMovieById = (id) => {
+   return `${baseUrl}/movie/${id}?api_key=${apiKey}&language=en-US`;
+}
+
+export { requests, getMovieImage, getMovieById };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
-import {requests, getMovieImage} from "../api/Requests";
+import {requests, getMovieImage, getMovieById} from "../api/Requests";
+import Hero from "../components/Hero";
 
 const Home = () => {
    const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
    const [trendingMovies, setTrendingMovies] = useState([]);
    const [upcomingMovies, setUpcomingMovies] = useState([]);
+   const [selectedMovie, setNewSelectedMovie] = useState({});
 
    useEffect(() => {
       const fetchMovieData = async () => {
          await axios.get(requests.trendingMovies).then((response) => {
             setTrendingMovies(response.data.results);
+            setNewSelectedMovie(response.data.results[0]);
          });
 
          await axios.get(requests.nowPlayingMovies).then((response) => {
@@ -25,11 +28,17 @@ const Home = () => {
       fetchMovieData();
    }, []);
 
+   const fetchSelectedMovie = async (id) => {
+      await axios.get(getMovieById(id)).then((response) => {
+         setNewSelectedMovie(response.data);
+      })
+   }
+
    const MovieList = ({ movies }) => {
       return movies.map((movie, i) => {
          return(
             <div className="inline-block px-2" key={i}>
-               <img className="rounded w-28 h-44" src={getMovieImage(movie.poster_path)}/>
+               <img className="rounded w-28 h-44 cursor-pointer" onClick={() => fetchSelectedMovie(movie.id)} src={getMovieImage(movie.poster_path)}/>
             </div>
          );
       });
@@ -37,19 +46,17 @@ const Home = () => {
 
    return (
       <div className="bg-black font-bold text-white p-5">
-         <header className="pb-10">
+         <header>
             <div className='flex items-center justify-between'>
                <a href="/home" className="text-5xl px-1 text-red-600 font-bold font-netflix-sans tracking-widest">NITFLIX</a>
-               <div>
+               {/* <div>
                   <input type="text" placeholder="Search your movie..." className="rounded mx-1 px-2 py-1 text-black"/>
                   <a href='' className="rounded bg-red-600 hover:bg-red-700 active:bg-red-800 text-white px-2 py-1.5">🔍</a>
-               </div>
+               </div> */}
             </div>
          </header>
 
-         <div>
-            {/* hero section */}
-         </div>
+         <Hero movie={selectedMovie}/>
 
          <div className="pb-5">
             <h3 className="text-2xl pl-2 pb-3">Trending Now</h3>
