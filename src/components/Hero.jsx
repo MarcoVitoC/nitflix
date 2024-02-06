3c426f619e1b980546eff6554d595234ed957080
MarcoVitoC <marcovito.chr@gmail.com>
2024-02-06 15:06:43 +0800

    feat: add button toggle for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,17 @@
+import {useState} from "react";
 import YouTube from "react-youtube";
 
 const Hero = ({ movie, trailerKey }) => {
    const baseBackdropUrl = import.meta.env.VITE_BASEIMGURL;
+   const [isPlayed, setPlayStatus] = useState(false);
+
+   const handlePlayButton = () => {
+      setPlayStatus(true);
+   }
+
+   const handleCloseButton = () => {
+      setPlayStatus(false);
+   }
 
    return(
       <div className="mt-5 mb-10 px-2 flex flex-col-reverse items-center rounded bg-cover h-auto lg:flex-row lg:h-[76vh]" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${baseBackdropUrl}${movie.backdrop_path})` }}>
@@ -9,17 +19,21 @@ const Hero = ({ movie, trailerKey }) => {
             <h1 className="text-5xl">{movie.title}</h1>
             <p className="text-xl font-medium py-5">{movie.overview}</p>
             <div className="self-center lg:self-start">
-               <button className="rounded bg-white hover:bg-slate-200 active:bg-slate-300 text-black px-5 py-2">
+               <button className="rounded bg-white hover:bg-slate-200 active:bg-slate-300 text-black px-5 py-2" onClick={handlePlayButton}>
                   ▶️ Play
                </button>
-               <button className="rounded bg-white hover:bg-slate-200 active:bg-slate-300 text-black px-5 ml-2 py-2">
-                  ❌ Close
-               </button>
+               {isPlayed && (
+                  <button className="rounded bg-white hover:bg-slate-200 active:bg-slate-300 text-black px-5 ml-2 py-2"  onClick={handleCloseButton}>
+                     ❌ Close
+                  </button>
+               )}
             </div>
          </div>
-         <div className="p-5">
-            <YouTube videoId={trailerKey} />
-         </div>
+         {isPlayed && (
+            <div className="p-5">
+               <YouTube videoId={trailerKey} />
+            </div>
+         )}
       </div>
    );
 }
